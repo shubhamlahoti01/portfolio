107dec7140435ea6e01f946baf6317515472a69d
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-07-02 18:30:49 +0530

    first

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div className="container portfolio-page">
+      <h1>My Work & Projects</h1>
+      <p>WORK IN PROGRESS...</p>
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./styles/Sidebar.scss";
+
+import LogoS from "../assets/images/logo-s.png";
+// import LogoSubtitle from "../assets/images/shubham.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faClose,
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to={"/"} onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <h3 className="sub-logo">SHUBHAM</h3>
+      </Link>
+      <nav className={showNav ? "mobile-show" : ""}>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className={"about-link"}
+          to="/about"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/shubham-lahoti/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/shubhamlahoti01"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/its.shubham1"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faInstagram}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  );
+};
+
+export default Sidebar;
